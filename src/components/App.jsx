5955ba72d696e3dbbe21b3203da457ad9c451397
Andrey Tsapkov <tsapkov1994@gmail.com>
2022-09-16 03:14:49 +0300

    init

---
+++ src/components/App.jsx
@@ -0,0 +1,7 @@
+import { useState, useEffect } from 'react';
+import { PropTypes } from 'prop-types';
+import { AppContainer } from './App.styled';
+
+export const App = () => {
+  return <AppContainer></AppContainer>;
+};
