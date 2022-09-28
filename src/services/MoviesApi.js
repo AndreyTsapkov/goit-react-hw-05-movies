e90876d93f505eecda87c9e94a446bc4dffbdd6a
Andrey Tsapkov <tsapkov1994@gmail.com>
2022-09-28 23:49:06 +0300

    init

--- src/services/MoviesApi.js
+++ src/services/MoviesApi.js
@@ -1,8 +1,60 @@
 import axios from 'axios';
+import {
+  normalizationMovieObj,
+  normalizationMoviesDetails,
+} from './normalizationObj';
 
 const API_KEY = 'e5517ae4303f5ec2cd4c7059ec556fc9';
 axios.defaults.baseURL = 'https://api.themoviedb.org/3';
 axios.defaults.params = {
   api_key: API_KEY,
   language: 'en-US',
+};
+
+export const fetchTrending = async () => {
+  return await axios.get(`trending/movie/day?api_key=${API_KEY}`);
+};
+
+export const fetchMovieDetails = async id => {
+  return await axios.get(`movie/${id}?api_key=${API_KEY}`);
+};
+
+export const fetchMovieCredits = async id => {
+  return await axios.get(`movie/${id}/credits?api_key=${API_KEY}`);
+};
+
+export const fetchMovieReviews = async id => {
+  return await axios.get(`movie/${id}/reviews?api_key=${API_KEY}`);
+};
+
+export const fetchMoviesByName = async name => {
+  return await axios.get(`search/movie?api_key=${API_KEY}&query=${name}`);
+};
+
+export const fetchTrendingMovies = async () => {
+  const { data } = await axios.get(`/trending/all/day?`);
+  const response = normalizationMovieObj(data.results);
+  return response;
+};
+
+export const fetchMoviesById = async id => {
+  const { data } = await axios.get(`/movie/${id}?`);
+  const response = normalizationMoviesDetails(data);
+  return response;
+};
+
+export const fetchMoviesBySearch = async query => {
+  const { data } = await axios.get(`/search/movie?query=${query}&page=1`);
+  const response = normalizationMovieObj(data.results);
+  return response;
+};
+
+export const fetchMoviesCredits = async id => {
+  const { data } = await axios.get(`/movie/${id}/credits?`);
+  return data.cast;
+};
+
+export const fetchMoviesReviews = async id => {
+  const { data } = await axios.get(`/movie/${id}/reviews?`);
+  return data.results;
 };
--- src/services/normalizationObj.js
+++ src/services/normalizationObj.js
@@ -1,21 +1,12 @@
 import genreList from './genreList';
-import settings from './settings';
 import noImg from '../images/noImageAvailable.jpg';
-
-const ssettings = {
-  API_KEY: '7cb8097836a7a1f4e5c19953961668c8',
-  BASE_URL: 'https://api.themoviedb.org/3',
-  IMG_URL: 'https://image.tmdb.org/t/p/w500/',
-};
-
-const { IMG_URL } = ssettings;
 
 const getGenreArray = genre_ids => genre_ids.map(el => genreList[`${el}`]);
 
 const getGenreArrayLibrary = genres => genres.map(el => el.name);
 
 export const getImgPath = imgPath =>
-  !imgPath ? `${noImg}` : `${IMG_URL}${imgPath}`;
+  !imgPath ? `${noImg}` : `https://image.tmdb.org/t/p/w500/${imgPath}`;
 
 const getDate = date => (!date ? date : date.slice(0, 4));
 
@@ -42,3 +33,22 @@ export const normalizationMovieObj = ({
   vote: vote_average,
   votes: vote_count,
 });
+
+export const normalizationMoviesDetails = ({
+  poster_path,
+  title,
+  original_title,
+  vote_average,
+  overview,
+  genres,
+  release_date,
+}) => {
+  return {
+    genre: getGenreArrayLibrary(genres),
+    title: original_title ? original_title : title,
+    about: overview,
+    img: getImgPath(poster_path),
+    releaseDate: release_date ? getDate(release_date) : '',
+    vote: vote_average,
+  };
+};
