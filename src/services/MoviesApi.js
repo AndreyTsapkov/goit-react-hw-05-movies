93aeb62b9808c4443b39de1d26458ff5d4c4d1fc
Andrey Tsapkov <tsapkov1994@gmail.com>
2022-09-29 23:58:46 +0300

    init

--- src/services/MoviesApi.js
+++ src/services/MoviesApi.js
@@ -11,26 +11,6 @@ axios.defaults.params = {
   language: 'en-US',
 };
 
-export const fetchTrending = async () => {
-  return await axios.get(`trending/movie/day?api_key=${API_KEY}`);
-};
-
-export const fetchMovieDetails = async id => {
-  return await axios.get(`movie/${id}?api_key=${API_KEY}`);
-};
-
-export const fetchMovieCredits = async id => {
-  return await axios.get(`movie/${id}/credits?api_key=${API_KEY}`);
-};
-
-export const fetchMovieReviews = async id => {
-  return await axios.get(`movie/${id}/reviews?api_key=${API_KEY}`);
-};
-
-export const fetchMoviesByName = async name => {
-  return await axios.get(`search/movie?api_key=${API_KEY}&query=${name}`);
-};
-
 export const fetchTrendingMovies = async () => {
   const { data } = await axios.get(`/trending/all/day?`);
   const response = normalizationMovieObj(data.results);
